0fc82e4182a6922b1ae6d31e15983b3a819c0490
Wendy Kurniawan <wiky.pedia@yahoo.com>
2023-01-05 14:17:31 +0700

    initial commit

---
+++ src/states/users/action.js
@@ -0,0 +1,26 @@
+import api from '../../utils/api'
+
+const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS'
+}
+
+function receiveUsersActionCreator (users) {
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: {
+      users
+    }
+  }
+}
+
+function asyncRegisterUser ({ id, name, password }) {
+  return async () => {
+    try {
+      await api.register({ id, name, password })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+}
+
+export { ActionType, receiveUsersActionCreator, asyncRegisterUser }
