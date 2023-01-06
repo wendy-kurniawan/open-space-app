0b892e91ae901df3456286d25c6b36c7a45c72d4
Wendy Kurniawan <wiky.pedia@yahoo.com>
2023-01-06 08:12:47 +0700

    make test pass

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import LoginInput from '../components/LoginInput'
 import { asyncSetAuthUser } from '../states/authUser/action'
 
-function LoginPage () {
+function LoginPage() {
   const dispatch = useDispatch() // @TODO: get dispatch function from store
 
   const onLogin = ({ id, password }) => {
@@ -23,7 +23,7 @@ function LoginPage () {
       <article className='login-page__main'>
         <h2>
           See <strong>The World</strong>, <br />
-          Through Open Space.
+          Through Open Space App.
         </h2>
 
         <LoginInput login={onLogin} />
